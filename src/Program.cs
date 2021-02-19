87e98b18b8fd2d184aa0a367e6f75a6326bf8d86
Aeris <aeris.github@gmail.com>
2021-02-19 17:46:40 +0100

    Moved core stuff in Program.cs

--- src/Program.cs
+++ src/Program.cs
@@ -1,13 +1,46 @@
-﻿using WaterBot.Discord;
+﻿using System;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using Microsoft.Extensions.Logging;
+using WaterBot.Commands;
+using WaterBot.Discord;
 
 namespace WaterBot
 {
-    public class Program
+    public static class Program
     {
+        private static DiscordClient _client;
+
         private static void Main(string[] args)
         {
-            using (DiscordBot bot = new DiscordBot("config.json"))
-                bot.RunAsync().GetAwaiter().GetResult();
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        private static async Task MainAsync()
+        {
+            _client = new DiscordClient(new DiscordConfiguration
+            {
+                Token = DiscordBotConfiguration.Token,
+                TokenType = TokenType.Bot,
+                MinimumLogLevel = LogLevel.Debug
+            });
+
+            var commands = _client.UseCommandsNext(new CommandsNextConfiguration
+            {
+                StringPrefixes = new []{"wbot!", "wb!", "water!"}
+            });
+
+            commands.RegisterCommands<GeneralCommandModule>();
+            commands.RegisterCommands<ConfigCommandModule>();
+
+            commands.CommandErrored += async (s, e) =>
+            {
+                Console.WriteLine(e.Exception);
+            };
+
+            await _client.ConnectAsync();
+            await Task.Delay(-1);
         }
     }
 }
