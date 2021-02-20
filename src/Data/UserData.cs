f1ed8060a1624dbb77031aa5f52841da1b942d43
Aeris <aeris.github@gmail.com>
2021-02-20 19:58:06 +0100

    Added RemindersList property and a method to get them

--- src/Data/UserData.cs
+++ src/Data/UserData.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace WaterBot.Data
 {
@@ -10,16 +11,43 @@ namespace WaterBot.Data
 
         public TimeSpan SleepTime { get; set; }
 
+        public TimeSpan UtcOffset { get; set; }
+
         public int AmountPerInterval { get; set; }
 
         public int AmountPerDay { get; set; }
 
         public bool ReminderEnabled { get; set; }
 
+        public List<TimeSpan> RemindersList { get; set; }
+
         public override string ToString()
         {
             return
                 $"UserID: {UserId}. WakeTime: {WakeTime}. SleepTime: {SleepTime}. AmountPerInterval: {AmountPerInterval}.";
+        }
+
+        public static IEnumerable<TimeSpan> CalculateReminders(UserData userData)
+        {
+            List<TimeSpan> remindersList = new List<TimeSpan>();
+
+            // ReSharper disable once PossibleLossOfFraction
+            int remindersNumber = (int)Math.Floor((double)(userData.AmountPerDay / userData.AmountPerInterval)) ;
+
+            TimeSpan remindersTimespan = userData.SleepTime - userData.WakeTime;
+
+            TimeSpan intervalBetweenEachReminder = remindersTimespan / remindersNumber;
+
+            TimeSpan progression = userData.WakeTime;
+            progression = progression.Subtract(new TimeSpan(0, 0, progression.Seconds, progression.Milliseconds));
+
+            while (progression < userData.SleepTime)
+            {
+                remindersList.Add(progression);
+                progression += intervalBetweenEachReminder;
+            }
+
+            return remindersList;
         }
     }
 }
