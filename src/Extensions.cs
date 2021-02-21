21ef1fc41a337c2ccad5b53b15e69b9ba086b3e8
Aeris <aeris.github@gmail.com>
2021-02-21 14:42:14 +0100

    Added Extensions.cs and a method to divide a string in chunks

---
+++ src/Extensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace WaterBot
+{
+    public static class Extensions
+    {
+        public static IEnumerable<IEnumerable<char>> ChunkBy(this string input, int n)
+        {
+            var list = new List<char>();
+            int i = 0;
+            foreach (var element in input)
+            {
+                list.Add(element);
+                i++;
+                if (i == n)
+                {
+                    yield return list;
+                    list = new List<char>();
+                    i = 0;
+                }
+            }
+            if (i != 0)
+                yield return list;
+        }
+    }
+}
