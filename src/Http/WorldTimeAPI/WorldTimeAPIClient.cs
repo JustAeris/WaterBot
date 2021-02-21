fdf9ef97768b3639794c458f9fc2eea7119185b8
Aeris <aeris.github@gmail.com>
2021-02-21 14:44:54 +0100

    Refactoring and added a new method to get all of the regions available inside a region

--- src/Http/WorldTimeAPI/WorldTimeAPIClient.cs
+++ src/Http/WorldTimeAPI/WorldTimeAPIClient.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -6,13 +7,13 @@ using Newtonsoft.Json.Linq;
 
 namespace WaterBot.Http.WorldTimeAPI
 {
-    public class WorldTimeAPIClient : IDisposable
+    public class WorldTimeApiClient : IDisposable
     {
         public const string BaseUrl = "http://worldtimeapi.org/api/timezone/";
 
         private HttpClient _client;
 
-        public WorldTimeAPIClient()
+        public WorldTimeApiClient()
         {
             _client = new HttpClient();
         }
@@ -29,17 +30,31 @@ namespace WaterBot.Http.WorldTimeAPI
             JObject data = JObject.Parse(content);
 
             // Read some values from the parsed JSON object
-            string timezone = data["timezone"].ToString();
-            int offset = int.Parse(data["raw_offset"].ToString());
+            string timezone = data["timezone"]?.ToString();
+            int offset = (int) data["raw_offset"];
 
-            string[] split = timezone.Split('/');
+            string[] split = timezone?.Split('/');
 
-            if (split.Length != 2)
+            if (split != null && split.Length != 2)
                 throw new FormatException($"Input string {timezone} is invalid");
 
             // Return the time zone with split values. The raw offset is returned
             // in seconds, so let's turn that into hours!
-            return new TimeZoneResponse(split[0], split[1], offset / 3600);
+            return new TimeZoneResponse(split?[0], split?[1], offset / 3600);
+        }
+
+        public async Task<IEnumerable<string>> GetRegions(string region)
+        {
+            HttpResponseMessage response = await _client.GetAsync(BaseUrl + region);
+
+            if (response.StatusCode != HttpStatusCode.OK)
+                throw new Exception($"Could not get regions for {region}: HTTP {(int)response.StatusCode}");
+
+            // Get the content from the response and turn it into a JSON object
+            string content = await response.Content.ReadAsStringAsync();
+            JArray data = JArray.Parse(content);
+
+            return data.ToObject<List<string>>();
         }
 
         public void Dispose()
