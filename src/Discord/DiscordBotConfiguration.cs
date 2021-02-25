1692f491bedc4a656ff4d27fef540d91e4a6a347
Aeris <aeris.github@gmail.com>
2021-02-25 11:18:15 +0100

    Added a new class and new settings for custom emojis

--- src/Discord/DiscordBotConfiguration.cs
+++ src/Discord/DiscordBotConfiguration.cs
@@ -14,9 +14,31 @@ namespace WaterBot.Discord
 
             Token = (string) content["Token"];
             DataDir = (string) content["DataDir"];
+
+            UseCustomEmojis = (bool) content["CustomEmojis"]?["UseCustomEmojis"];
+
+            if (!UseCustomEmojis) return;
+            CustomEmojis.DropletMain = (string) content["CustomEmojis"]?["DropletMain"];
+            CustomEmojis.DropletCheck = (string) content["CustomEmojis"]?["DropletCheck"];
+            CustomEmojis.DropletCross = (string) content["CustomEmojis"]?["DropletCross"];
+            CustomEmojis.DropletTrophy = (string) content["CustomEmojis"]?["DropletTrophy"];
+            CustomEmojis.DropletFire = (string) content["CustomEmojis"]?["DropletFire"];
+            CustomEmojis.DropletWarning = (string) content["CustomEmojis"]?["DropletWarning"];
         }
 
         public static string Token { get; }
         public static string DataDir { get; }
+
+        public static bool UseCustomEmojis { get; }
+
+        public static class CustomEmojis
+        {
+            public static string DropletMain { get; set; }
+            public static string DropletCheck { get; set; }
+            public static string DropletCross { get; set; }
+            public static string DropletTrophy { get; set; }
+            public static string DropletFire { get; set; }
+            public static string DropletWarning { get; set; }
+        }
     }
 }
